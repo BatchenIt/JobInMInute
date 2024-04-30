b94da79094a307a4935b6609a71457430f4c599d
Gilad <gilad.skop@gmail.com>
2024-04-30 14:54:51 +0300

    init

---
+++ JobInMinuteServer/Controllers/JobsController.cs
@@ -0,0 +1,51 @@
+﻿using JobInMinuteServer.DAL.Interfaces;
+using JobInMinuteServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobInMinuteServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobsController : Controller
+    {
+
+        private readonly IJobRepository _jobRepository;
+
+        public JobsController(IJobRepository jobRepository)
+        {
+            _jobRepository = jobRepository;
+        }
+
+
+        [HttpPost(Name = "saveJob")]
+        public async Task<IActionResult> SaveJob([FromBody] Job job)
+        {
+            try
+            {
+                await _jobRepository.SaveJob(job);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+
+        }
+
+        [HttpGet(Name = "getJob")]
+        public async Task<IActionResult> GetJobById(int jobId)
+        {
+            try
+            {
+                Job job = await _jobRepository.GetJobById(jobId);
+                return Ok(job);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ JobInMinuteServer/DAL/JobRepository.cs
@@ -0,0 +1,27 @@
+﻿using JobInMinuteServer.DAL.Interfaces;
+using JobInMinuteServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobInMinuteServer.DAL
+{
+    public class JobRepository: IJobRepository
+    {
+        private readonly JobInMinuteDbContext _context;
+        public JobRepository(JobInMinuteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveJob(Job job)
+        {
+            
+            _context.Jobs.Add(job);
+          
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Job> GetJobById(int jobId)
+        {
+            return await _context.Jobs.FindAsync(jobId);
+        }
+    }
+}
